75fb2bdaaef65d337d6618efeb20c7842bbca394
Hasan <blackbee08@gmail.com>
2021-04-01 03:48:50 +0700

    Initial Commit

---
+++ src/Eatigo.Eatilink.Api/Controllers/LinkShortenerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eatigo.Eatilink.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LinkShortenerController : ControllerBase
+    {
+        [HttpGet("shorten")]
+        public IActionResult LinkShortener() 
+        {
+            return StatusCode(StatusCodes.Status200OK, "Hasan");
+        }
+    }
+}
